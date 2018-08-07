c177e07ce5b5741d62479108a4e874f18c31142a
Chris Rougraff <croug327@gmail.com>
2018-08-07 14:34:53 -0400

    Problem 1 complete

---
+++ src/test/java/com/revature/eval/java/core/EvaluationLauncher.java
@@ -0,0 +1,8 @@
+package com.revature.eval.java.core;
+
+public class EvaluationLauncher {
+	public static void main(String[] args) {
+		EvaluationService es = new EvaluationService();
+		es.acronym("Core Java");
+	}
+}
